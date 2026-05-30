7a7d9c933866e9881592f7eaed58ebd2c3947dab
Zarifa <zarifarahmani99@gmail.com>
2026-05-30 10:18:17 +0430

    Install dependencies and update ThemeProvider
    Add package.json, package-lock.json and the installed node_modules (including D3, React, React DOM, topojson-client, next-themes, prop-types and related packages) as part of a dependency install. Also modify world-explorer/components/ThemeProvider.jsx to accommodate the newly added libraries and updated theme handling.

--- world-explorer/components/ThemeProvider.jsx
+++ world-explorer/components/ThemeProvider.jsx
@@ -4,7 +4,11 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export default function ThemeProvider({ children }) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+    >
       {children}
     </NextThemesProvider>
   );
